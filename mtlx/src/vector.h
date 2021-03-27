e73ba4125e361f21e87ea8411ac0a7a00e902a1b
Christian Panov <krissko7209@abv.bg>
2021-03-27 19:09:02 +0200

    Added more useful aliases for different types than floating points

--- mtlx/src/vector.h
+++ mtlx/src/vector.h
@@ -73,6 +73,21 @@ namespace mtlx
 	using vec2 = vec<2, float>;
 	using vec3 = vec<3, float>;
 	using vec4 = vec<4, float>;
+
+	using vec1i = vec<1, int>;
+	using vec2i = vec<2, int>;
+	using vec3i = vec<3, int>;
+	using vec4i = vec<4, int>;
+
+	using vec1u = vec<1, unsigned int>;
+	using vec2u = vec<2, unsigned int>;
+	using vec3u = vec<3, unsigned int>;
+	using vec4u = vec<4, unsigned int>;
+
+	using vec1b = vec<1, bool>;
+	using vec2b = vec<2, bool>;
+	using vec3b = vec<3, bool>;
+	using vec4b = vec<4, bool>;
 }
 
 #include "vector_impl.h"
