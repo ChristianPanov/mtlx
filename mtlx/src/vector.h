dfabaf2f25afe778ce4e60725d5b98a62fa5f566
Christian Panov <krissko7209@abv.bg>
2021-03-27 18:51:14 +0200

    Moved everything into an mtlx namespace

--- mtlx/src/vector.h
+++ mtlx/src/vector.h
@@ -3,48 +3,48 @@
 #include <cstdint>
 #include <type_traits>
 
-template<
-	std::uint8_t Dims,
-	typename Type = float
->
-struct vector
+namespace mtlx
 {
-public:
-	template<typename... Args,
-		typename = std::enable_if_t<sizeof...(Args) == Dims>>
-	vector(Args&&... args);
+	template<std::uint8_t Dims, typename Type = float>
+	struct vector
+	{
+	public:
+		template<typename... Args,
+			typename = std::enable_if_t<sizeof...(Args) == Dims>>
+			vector(Args&&... args);
 
-public:
-	Type& operator[](std::uint8_t index);
-	const Type& operator[](std::uint8_t index) const;
+	public:
+		Type& operator[](std::uint8_t index);
+		const Type& operator[](std::uint8_t index) const;
 
-public:
-	vector& operator+=(const vector& other);
-	vector& operator-=(const vector& other);
-	vector& operator*=(Type scalar);
-	vector& operator/=(Type scalar);
-	vector& operator-();
+	public:
+		vector& operator+=(const vector& other);
+		vector& operator-=(const vector& other);
+		vector& operator*=(Type scalar);
+		vector& operator/=(Type scalar);
+		vector& operator-();
 
-public:
-	Type values[Dims]{};
-};
+	public:
+		Type values[Dims]{};
+	};
 
-template<std::uint8_t Dims, typename Type>
-vector<Dims, Type> operator+(const vector<Dims, Type>& vec1, const vector<Dims, Type>& vec2);
+	template<std::uint8_t Dims, typename Type>
+	vector<Dims, Type> operator+(const vector<Dims, Type>& vec1, const vector<Dims, Type>& vec2);
 
-template<std::uint8_t Dims, typename Type>
-vector<Dims, Type> operator-(const vector<Dims, Type>& vec1, const vector<Dims, Type>& vec2);
+	template<std::uint8_t Dims, typename Type>
+	vector<Dims, Type> operator-(const vector<Dims, Type>& vec1, const vector<Dims, Type>& vec2);
 
-template<std::uint8_t Dims, typename Type>
-vector<Dims, Type> operator*(const vector<Dims, Type>& vec, Type scalar);
+	template<std::uint8_t Dims, typename Type>
+	vector<Dims, Type> operator*(const vector<Dims, Type>& vec, Type scalar);
 
-template<std::uint8_t Dims, typename Type>
-vector<Dims, Type> operator/(const vector<Dims, Type>& vec, Type scalar);
+	template<std::uint8_t Dims, typename Type>
+	vector<Dims, Type> operator/(const vector<Dims, Type>& vec, Type scalar);
 
-template<std::uint8_t Dims, typename Type>
-Type magnitude(const vector<Dims, Type>& vec);
+	template<std::uint8_t Dims, typename Type>
+	Type magnitude(const vector<Dims, Type>& vec);
 
-template<std::uint8_t Dims, typename Type>
-vector<Dims, Type> normalize(const vector<Dims, Type>& vec);
+	template<std::uint8_t Dims, typename Type>
+	vector<Dims, Type> normalize(const vector<Dims, Type>& vec);
+}
 
 #include "vector_impl.h"
--- mtlx/src/vector_impl.h
+++ mtlx/src/vector_impl.h
@@ -2,119 +2,122 @@
 
 #include "vector.h"
 
-template<std::uint8_t Dims, typename Type>
-template<typename ...Args, typename>
-vector<Dims, Type>::vector(Args&&... args)
-	: values{ std::forward<Args>(args)... }
-{}
-
-template<std::uint8_t Dims, typename Type>
-Type& vector<Dims, Type>::operator[](std::uint8_t index)
+namespace mtlx
 {
-	return values[index];
-}
+	template<std::uint8_t Dims, typename Type>
+	template<typename ...Args, typename>
+	vector<Dims, Type>::vector(Args&&... args)
+		: values{ std::forward<Args>(args)... }
+	{}
 
-template<std::uint8_t Dims, typename Type>
-const Type& vector<Dims, Type>::operator[](std::uint8_t index) const
-{
-	return values[index];
-}
+	template<std::uint8_t Dims, typename Type>
+	Type& vector<Dims, Type>::operator[](std::uint8_t index)
+	{
+		return values[index];
+	}
 
-template<std::uint8_t Dims, typename Type>
-vector<Dims, Type>& vector<Dims, Type>::operator+=(const vector& other)
-{
-	for (std::uint8_t i = 0; i < Dims; ++i)
-		values[i] += other[i];
-	return *this;
-}
+	template<std::uint8_t Dims, typename Type>
+	const Type& vector<Dims, Type>::operator[](std::uint8_t index) const
+	{
+		return values[index];
+	}
 
-template<std::uint8_t Dims, typename Type>
-vector<Dims, Type>& vector<Dims, Type>::operator-=(const vector& other)
-{
-	for (std::uint8_t i = 0; i < Dims; ++i)
-		values[i] -= other[i];
-	return *this;
-}
-
-template<std::uint8_t Dims, typename Type>
-vector<Dims, Type>& vector<Dims, Type>::operator*=(Type scalar)
-{
-	for (std::uint8_t i = 0; i < Dims; ++i)
-		values[i] *= scalar;
-	return *this;
-}
-
-template<std::uint8_t Dims, typename Type>
-vector<Dims, Type>& vector<Dims, Type>::operator/=(Type scalar)
-{
-	scalar = 1.0f / scalar;
-	for (std::uint8_t i = 0; i < Dims; ++i)
-		values[i] *= scalar;
-	return *this;
-}
-
-template<std::uint8_t Dims, typename Type>
-vector<Dims, Type>& vector<Dims, Type>::operator-()
-{
-	for (std::uint8_t i = 0; i < Dims; ++i)
-		values[i] *= -1;
-	return *this;
-}
-
-template<std::uint8_t Dims, typename Type>
-vector<Dims, Type> operator+(const vector<Dims, Type>& vec1, const vector<Dims, Type>& vec2)
-{
-	return [&]() {
+	template<std::uint8_t Dims, typename Type>
+	vector<Dims, Type>& vector<Dims, Type>::operator+=(const vector& other)
+	{
 		for (std::uint8_t i = 0; i < Dims; ++i)
-			vec1[i] += vec2[i];
-		return vector<Dims, Type>(vec1);
-	}();
-}
+			values[i] += other[i];
+		return *this;
+	}
 
-template<std::uint8_t Dims, typename Type>
-vector<Dims, Type> operator-(const vector<Dims, Type>& vec1, const vector<Dims, Type>& vec2)
-{
-	return [&]() {
+	template<std::uint8_t Dims, typename Type>
+	vector<Dims, Type>& vector<Dims, Type>::operator-=(const vector& other)
+	{
 		for (std::uint8_t i = 0; i < Dims; ++i)
-			vec1[i] -= vec2[i];
-		return vector<Dims, Type>(vec1);
-	}();
-}
+			values[i] -= other[i];
+		return *this;
+	}
 
-template<std::uint8_t Dims, typename Type>
-vector<Dims, Type> operator*(const vector<Dims, Type>& vec, Type scalar)
-{
-	return [&]() {
+	template<std::uint8_t Dims, typename Type>
+	vector<Dims, Type>& vector<Dims, Type>::operator*=(Type scalar)
+	{
 		for (std::uint8_t i = 0; i < Dims; ++i)
-			vec[i] *= scalar;
-		return vector<Dims, Type>(vec);
-	}();
-}
+			values[i] *= scalar;
+		return *this;
+	}
 
-template<std::uint8_t Dims, typename Type>
-vector<Dims, Type> operator/(const vector<Dims, Type>& vec, Type scalar)
-{
-	return [&]() {
+	template<std::uint8_t Dims, typename Type>
+	vector<Dims, Type>& vector<Dims, Type>::operator/=(Type scalar)
+	{
 		scalar = 1.0f / scalar;
 		for (std::uint8_t i = 0; i < Dims; ++i)
-			vec[i] *= scalar;
-		return vector<Dims, Type>(vec);
-	}();
-}
+			values[i] *= scalar;
+		return *this;
+	}
 
-template<std::uint8_t Dims, typename Type>
-Type magnitude(const vector<Dims, Type>& vec)
-{
-	return [vec]() {
-		Type mag{};
+	template<std::uint8_t Dims, typename Type>
+	vector<Dims, Type>& vector<Dims, Type>::operator-()
+	{
 		for (std::uint8_t i = 0; i < Dims; ++i)
-			mag += vec[i] * vec[i];
-		return std::sqrt(mag);
-	}();
-}
+			values[i] *= -1;
+		return *this;
+	}
 
-template<std::uint8_t Dims, typename Type>
-vector<Dims, Type> normalize(const vector<Dims, Type>& vec)
-{
-	return (vec / magnitude(vec));
+	template<std::uint8_t Dims, typename Type>
+	vector<Dims, Type> operator+(const vector<Dims, Type>& vec1, const vector<Dims, Type>& vec2)
+	{
+		return [&]() {
+			for (std::uint8_t i = 0; i < Dims; ++i)
+				vec1[i] += vec2[i];
+			return vector<Dims, Type>(vec1);
+		}();
+	}
+
+	template<std::uint8_t Dims, typename Type>
+	vector<Dims, Type> operator-(const vector<Dims, Type>& vec1, const vector<Dims, Type>& vec2)
+	{
+		return [&]() {
+			for (std::uint8_t i = 0; i < Dims; ++i)
+				vec1[i] -= vec2[i];
+			return vector<Dims, Type>(vec1);
+		}();
+	}
+
+	template<std::uint8_t Dims, typename Type>
+	vector<Dims, Type> operator*(const vector<Dims, Type>& vec, Type scalar)
+	{
+		return [&]() {
+			for (std::uint8_t i = 0; i < Dims; ++i)
+				vec[i] *= scalar;
+			return vector<Dims, Type>(vec);
+		}();
+	}
+
+	template<std::uint8_t Dims, typename Type>
+	vector<Dims, Type> operator/(const vector<Dims, Type>& vec, Type scalar)
+	{
+		return [&]() {
+			scalar = 1.0f / scalar;
+			for (std::uint8_t i = 0; i < Dims; ++i)
+				vec[i] *= scalar;
+			return vector<Dims, Type>(vec);
+		}();
+	}
+
+	template<std::uint8_t Dims, typename Type>
+	Type magnitude(const vector<Dims, Type>& vec)
+	{
+		return [vec]() {
+			Type mag{};
+			for (std::uint8_t i = 0; i < Dims; ++i)
+				mag += vec[i] * vec[i];
+			return std::sqrt(mag);
+		}();
+	}
+
+	template<std::uint8_t Dims, typename Type>
+	vector<Dims, Type> normalize(const vector<Dims, Type>& vec)
+	{
+		return (vec / magnitude(vec));
+	}
 }
